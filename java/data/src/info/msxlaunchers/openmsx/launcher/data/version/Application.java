c569740d7ff619fa64dc54b7acc211ac3813b1b7
Sam Elsharif <oelsha@engineer.com>
2016-07-25 23:06:08 -0400

    updated for version 1.7

--- java/data/src/info/msxlaunchers/openmsx/launcher/data/version/Application.java
+++ java/data/src/info/msxlaunchers/openmsx/launcher/data/version/Application.java
@@ -24,7 +24,7 @@ package info.msxlaunchers.openmsx.launcher.data.version;
  */
 public interface Application
 {
-	public final static String VERSION = "1.6";
+	public final static String VERSION = "1.7";
 
 	public final static String RELEASE_YEARS = "2013-2016";
 }
