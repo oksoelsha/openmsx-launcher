58a03e0817d2cbb321b0c62e427f232cf91a38ab
Sam Elsharif <oelsha@engineer.com>
2018-12-14 20:07:25 -0500

    updated for version 1.10.6

--- java/common/src/info/msxlaunchers/openmsx/common/version/Application.java
+++ java/common/src/info/msxlaunchers/openmsx/common/version/Application.java
@@ -26,7 +26,7 @@ public interface Application
 {
 	public static final String APP_NAME = "openMSX Launcher";
 
-	public static final String VERSION = "1.10.5";
+	public static final String VERSION = "1.10.6";
 
 	public static final String RELEASE_YEARS = "2013-2018";
 }
